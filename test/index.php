aff7998c6d597463657bd7bbb1c68edcadb7d9f3
Chrystian Ramos <chrystian_ramos@live.com>
2018-10-11 16:32:04 -0300

    Test fix

--- test/index.php
+++ test/index.php
@@ -3,9 +3,10 @@ require __DIR__ . '/../lib-ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(2, "caxote.com.br", "sender@caxote.com.br", "teste@123", "tls", "587", "chrystian@caxote.com.br", "Equipe Caxote");
-$novoEmail->sendMail("Assunto de Teste", "<p>Esse é um e-mail de <b>TESTE</b>!</p>",
-    "chrystian@caxote.com.br", "Caxote Web", "chrystian_ramos@live.com",
-    "Chrystian Ramos");
+$mail = new Email(2, "mail.host.com", "your@email.com", "your-pass",
+    "smtp secure (tls / ssl)", "port (587)", "from@email.com", "From Name");
+
+$email->sendEmail("Subject", "Content", "reply@email.com", "Replay 
+Name", "address@email.com", "Address Name");
 
 var_dump($novoEmail);
